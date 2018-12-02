39973dc287978e8a9ad4e99434361bdc9e20340a
xuxuewen0422 <44302970+xuxuewen0422@users.noreply.github.com>
2018-12-02 14:49:11 -0500

    update

--- src/Reproduction.java
+++ src/Reproduction.java
@@ -1,4 +1,6 @@
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -6,39 +8,65 @@ import java.util.Random;
 
 public class Reproduction {
 
-    public static List<Chromosome> getNextGeneration(List<Chromosome> list){return list;}
-
-    private static Chromosome getNextGeneration(Chromosome fa , Chromosome mo){
-        return null;
+    public static List<Chromosome> getNextGeneration(List<Chromosome> list){
+        List<Chromosome> newlist = new ArrayList<Chromosome>();
+        for(int i=0;i<list.size();i++) {
+        List<Chromosome> temlist = CollocationDegree.fitness(list);
+        Chromosome fa = temlist.get(0);
+        Chromosome ma = temlist.get(1);
+        //List<Chromosome> newlist = new ;
+        Chromosome son1 = getNextGeneration(fa,ma);
+        Chromosome son2 = getNextGeneration(fa,ma);
+        newlist.add(son1);
+        newlist.add(son2);
+        }
+        int N = newlist.size();
+        newlist.sort(null);
+        newlist = newlist.subList(N/2,N);
+        return newlist;
     }
 
-    private static Chromosome mutate(Chromosome fa , Chromosome mo){
-        Chromosome son = crossover( fa , mo);
-        Random random = new Random();
-        int N=fa.getGenes().size();
+    private static Chromosome getNextGeneration(Chromosome fa , Chromosome mo){
+        Chromosome son;
+        son = crossover(fa , mo);
+        son = mutate(son);
+        return son;
+    }
 
-        int times = random.nextInt(N);
-        for(int i=0; i<times; i++)
+    private static Chromosome mutate(Chromosome son){
+        Random random = new Random();
+        int N=0;
+        for (Server item : son.getGenes()) {
+            N++;
+        }
+        //int times = random.nextInt(N);
+        for(int i=0; i<N; i++)
         {
-            int posit = random.nextInt(N);
-            int number = random.nextInt(GAConfiguration.server_exceeding_speed_range);
+            //int posit = random.nextInt(N);
+            int chance = random.nextInt(100);
+            if(chance<3){
+            int number = random.nextInt(20);
             Server server = new Server(N,number);
-            son.getGenes().set(N, server);
+            son.getGenes().set(i, server);}
         }
 
         return son;
     }
 
     private static Chromosome crossover(Chromosome fa , Chromosome mo){
-        int N= fa.getGenes().size();
+        int N=0;
         Chromosome son = new Chromosome();
         Random random = new Random();
+        for (Server item : fa.getGenes()) {
+            N++;
+                    }
+        Iterator<Server> he = fa.getGenes().iterator();
+        Iterator<Server> she = mo.getGenes().iterator();
         int c = random.nextInt(2);
 
         if(c==0){
             son.getGenes().addAll(fa.getGenes().subList(0,N/2));
             son.getGenes().addAll(mo.getGenes().subList(N/2,N));
-
         }
         else
         {
@@ -47,5 +75,7 @@ public class Reproduction {
         }
         return son;
     }
+
+
 }
 
