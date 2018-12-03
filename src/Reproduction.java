e5de6920aa234c39ad16e156aecdf50b1317c915
xuxuewen0422 <44302970+xuxuewen0422@users.noreply.github.com>
2018-12-02 20:02:22 -0500

    update

--- src/Reproduction.java
+++ src/Reproduction.java
@@ -8,15 +8,17 @@ import java.util.Random;
 
 public class Reproduction {
 
-    public static List<Chromosome> getNextGeneration(List<Chromosome> list){
+    public static List<Chromosome> getNextGeneration(List<Chromosome> list, List<Server> server, List<Task> task){
         List<Chromosome> newlist = new ArrayList<Chromosome>();
         for(int i=0;i<list.size();i++) {
         List<Chromosome> temlist = CollocationDegree.fitness(list);
         Chromosome fa = temlist.get(0);
         Chromosome ma = temlist.get(1);
         //List<Chromosome> newlist = new ;
-        Chromosome son1 = getNextGeneration(fa,ma);
-        Chromosome son2 = getNextGeneration(fa,ma);
+        Chromosome son1 = getNextGeneration(fa,ma,server);
+        son1.setCollocationDegree(CollocationDegree.getCollocationDegreeByGenes(son1.getGenes(),task));
+        Chromosome son2 = getNextGeneration(fa,ma,server);
+        son2.setCollocationDegree(CollocationDegree.getCollocationDegreeByGenes(son2.getGenes(),task));
         newlist.add(son1);
         newlist.add(son2);
         }
@@ -26,28 +28,31 @@ public class Reproduction {
         return newlist;
     }
 
-    private static Chromosome getNextGeneration(Chromosome fa , Chromosome mo){
+    private static Chromosome getNextGeneration(Chromosome fa , Chromosome mo,List<Server> server){
         Chromosome son;
         son = crossover(fa , mo);
-        son = mutate(son);
+        son = mutate(son,server);
         return son;
     }
 
-    private static Chromosome mutate(Chromosome son){
+    private static Chromosome mutate(Chromosome son, List<Server> server){
         Random random = new Random();
+        int serverN = server.size();
+        Server newserver;
         int N=0;
         N = son.getGenes().size();
-        //int times = random.nextInt(N);
-        for(int i=0; i<N; i++)
-        {
-            //int posit = random.nextInt(N);
+        List<Server> newgene = new ArrayList<Server>();
+        for(int i=0; i<N; i++) {
             int chance = random.nextInt(100);
-            if(chance<3){
-            int number = random.nextInt(20);
-            Server server = new Server(N,number);
-            son.getGenes().set(i, server);}
+            if (chance < 3) {
+                int id = random.nextInt(serverN);
+                newserver = server.get(id);
+                newgene.add(newserver);
+            }
+            newserver = son.getGenes().get(i);
+            newgene.add(newserver);
         }
-
+        son.setGenes(newgene);
         return son;
     }
 
